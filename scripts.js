cf0a294417e358d946da4f88137eea942f7b98d2
Callum Govender <callumlevigovender@gmail.com>
2024-10-24 21:41:52 +0200

    code is more readable

--- scripts.js
+++ scripts.js
@@ -1,50 +1,54 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
-function crash(){
-  const body = document.getElementById('body');
-  body.innerHTML ='<h1>Something critical went wrong. Please reload the page</h1>'
-}
-
+function crash() {
+  const body = document.getElementById("body");
+  body.innerHTML =
+    "<h1>Something critical went wrong. Please reload the page</h1>";
+};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
- 
+
   let errors = false;
 
   try {
-    switch(true) {
+    switch (true) {
       case dividend === "":
-        result.innerText = 'Division not performed. Both values are required in inputs. Try again'; 
+        result.innerText =
+          "Division not performed. Both values are required in inputs. Try again";
         errors = true;
         break;
       case !/^\d+$/.test(dividend): // Check if dividend is numeric
-        result.innerText = 'Division not performed. Dividend must be a valid whole number. Try again';
+        result.innerText =
+          "Division not performed. Dividend must be a valid whole number. Try again";
         crash();
         console.error(new Error("Invalid parameter entered!"));
         errors = true;
         break;
       case !/^\d+$/.test(divider): // Check if divider is numeric
-        result.innerText = 'Division not performed. Divider must be a valid whole number. Try again';
+        result.innerText =
+          "Division not performed. Divider must be a valid whole number. Try again";
         crash();
         console.error(new Error("Invalid parameter entered!"));
         errors = true;
         break;
-      case divider === '':
-        result.innerText = 'Division not performed. Both values are required in inputs. Try again'; 
+      case divider === "":
+        result.innerText =
+          "Division not performed. Both values are required in inputs. Try again";
         errors = true;
         break;
-      case divider === '0':
-        result.innerText = 'Division not performed. Invalid whole number provided. Try again';
+      case divider === "0":
+        result.innerText =
+          "Division not performed. Invalid whole number provided. Try again";
         errors = true;
         console.error(new Error("The result is undefined as the divider is 0"));
       default:
         console.log("The Fields are filled. Entries are valid.");
     }
-  } finally{
-  
+  } finally {
     if (!errors) {
       result.innerText = Math.floor(dividend / divider);
     }
